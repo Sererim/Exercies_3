06ce50102bf5ff86d108ddcc303269e03d84254c
Sererim <danielleymanw@gmail.com>
2023-06-04 14:29:07 +0300

    Initial commit.

---
+++ red_and_black_binary_tree.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum node_color {
+    red,
+    black
+};
+
+typedef struct Node
+{
+
+} Node;
+
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
